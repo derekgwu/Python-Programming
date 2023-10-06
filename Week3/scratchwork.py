c509795e07b8c2bbe25ec2bec9c0359b6b3eeb8d
Derek Chen <dchen36@gwmail.gwu.edu>
2023-10-05 22:27:08 -0400

    adding scratchwork from today

---
+++ Week3/scratchwork.py
@@ -0,0 +1,62 @@
+x = 6
+y = 'c'
+z = "Hello World"
+a = True
+
+#####
+box = 6
+box = 5
+box2 = 9
+box = box2
+box2 = box
+print(box)
+print(box2)
+
+box3 = 2
+box3 = "Hello World"
+box3 = box3 + 1
+print(box3)
+
+if(box2 == box):
+    print("something")
+elif(box2 > box):
+    print("something else if")
+else:
+    print("something else")
+
+"""if(the sky is blue):
+        do something1
+    else if(the sky is yellow)
+        do something2
+    something3
+        
+    if(the sky is blue)
+        do something1
+    else
+        something2
+    
+    if(the entire sky is rainbow):
+        do something1
+        """
+a = 1
+b = 2
+c = 3
+d = 4
+
+if ((c > a and c > b) or c > d):
+    print("success")
+#box = 9
+#box2 = 9
+
+#multiple choice
+#true or false
+#a fill in the blank
+#a question that has more than one correct
+
+#optional:
+#a question that is two parts
+#where the second part depends on the first part
+#but if the user first part wrong, they cannot answer the second part
+
+# 4 * 5 = 20
+#using your answer from part a, what is 100 divided by the answer
