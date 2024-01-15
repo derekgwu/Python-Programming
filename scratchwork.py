dcc0dbe852b99ac55c70e8261874e495a8cd4b39
Derek Chen <dchen36@gwmail.gwu.edu>
2024-01-15 11:36:29 -0500

    added chapter 10

--- scratchwork.py
+++ scratchwork.py
@@ -1,29 +1,15 @@
-def swap(arr, num1, num2):
-    index = -1
-    for i in range(0, len(arr)):
-        if (arr[i] == num1 or arr[i] == num2) and index != -1:
-            temp = arr[index]
-            arr[index] = arr[i]
-            arr[i] = temp
-        if (arr[i] == num1 or arr[i] == num2):
-            index = i
-    return arr
+class Square():
+   def num_sides(this):
+       print("I have 4 sides")
 
+class Triangle():
+   def num_sides(this):
+       print("I have 3 sides")
 
+class Hexagon():
+   def num_sides(this):
+       print("I have 6 sides")
 
-
-def asserter(test_case, exp, func, *args):
-    try:
-        ret = func(*args)
-        if ret == exp:
-           print("Test Case " + str(test_case) + " SUCCESS!")
-        else:
-          print("Test Case " + str(test_case) + " FAILED. We expected: " + str(exp) + " but you returned " + str(ret))
-    except Exception as e:
-        print("Test Case " + str(test_case) + f" had an exception: {type(e).__name__}: {e}")
-
-asserter(1, [1,3,2], swap, [1,2,3], 2, 3)
-asserter(2, [2,1], swap, [1,2], 1, 2)
-asserter(3, [7,2,3,4,5,6,1], swap, [1,2,3,4,5,6,7], 1, 7)
-asserter(4, [1,2,5,4,3], swap, [1,2,3,4,5], 3, 5)
-asserter(5, [1,9,2,7], swap, [1,7,2,9], 7, 9)
+shapes = [Square(), Triangle(), Hexagon()]
+for shape in shapes:
+    shape.num_sides()
--- scratchwork2.py
+++ scratchwork2.py
@@ -1,29 +1,44 @@
-class Person():
-   def __init__(self, name, age):
-       self.name = name
-       self.age = age
-       #could have other things like birthday, hair color etc.
+import math
+import random
 
-   #let's add a method to show inheritance of methods as well
-   def get_name(this):
-       return this.name
-
-class Student(Person):
-    def __init__(self, name, age, grade):
-       super().__init__(name, age)
-       #you can add on additional variables, but always call the parent constructor first if needed    
-       self.grade = grade
+class Car():
+    def __init__(self, name, speed, control, weight):
+        self.name = name
+        self.speed = speed
+        self.control = control
+        self.weight = weight
     
-    def get_grade(this):
-       return this.grade
-    
-person_example = Person("Joe", 20)
-student_example = Student("Billy", 18, "12th")
-#this prints Joe
-person_example.get_name()
-#this prints Billy
-student_example.get_name()
+    def compute_time(this):
+        total_time = 0
+        for lap in range(0,50):
+            lap_time = (this.weight * 0.9) - (this.speed *0.8)
+            rand_control = 100 - this.control
+            lap_time += (random.randrange(1, rand_control))
+            total_time += lap_time
+            this.convert_time(total_time)
 
-#this prints out 12th
-student_example.get_grade()
-                
+    def convert_time(this, total_time):
+        totalmin = math.floor(total_time/60)
+        totalmin_str = str(totalmin)
+        if totalmin < 10:
+            totalmin_str = "0" + totalmin_str
+        totalsec = math.floor(total_time - (totalmin*60))
+        totalsec_str = str(totalsec)
+        if totalsec < 10:
+            totalsec_str = "0" + totalsec_str
+
+        print("CURRENT TIME: " + totalmin_str + ":" + totalsec_str)
+
+subaru = Car("Subaru", 49, 62, 98)
+subaru.compute_time()
+
+
+class Bullet(Car):
+    def __init__(self, name):
+        super().__init__(name, 70, 20, 40)
+
+#class Precision(Car):
+        
+#class Feather(Car):
+
+#def play():
