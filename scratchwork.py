16fbf968fc05c641d00f8536126150120e1c1081
Derek Chen <dchen36@gwmail.gwu.edu>
2024-03-04 16:56:06 -0500

    added this week material

--- scratchwork.py
+++ scratchwork.py
@@ -1,51 +1,45 @@
-#HANGMAN
-#do pip install random-word
-from random_word import RandomWords
+import random
 
-#gets a random word, returns in the form of _ _ _ _ ...
-def generate_word():
-    r = RandomWords()
-    word = r.get_random_word()
-    ret = ""
-    for i in word:
-        ret += " " + i
-    return ret
+#in the arr, all elements appear twice except for 1, return the element that appears once
+def appear_twice(arr):
+    for i in range(0, len(arr), 2):
+        if arr[i] != arr[i+1]:
+            return arr[i]
+    return -1
 
-def generate_blanks(word):
-    ret = ""
-    for i in word:
-        if i == 0:
-            ret += "_"
-        elif i != " ":
-            ret += " _"
-    return ret
+print(appear_twice([0,1,0,2,2]))
 
-guessed_letters = []
-word_to_guess = generate_word()
-print(word_to_guess)
-display_word = generate_blanks(word_to_guess)
-while display_word != word_to_guess:
-    printable = ""
-    for word in guessed_letters:
-        printable += word
-    
-    print("GUESSED LETTERS: " + printable)
-    print(display_word)
-    if(len(printable) >= 10):
-        print("YOU LOSE!")
-        exit()
-    guess = input("Please enter a letter.")
-    guessed_letters.append(guess)
-    
-    new_display = ""
-    for i in range (0, len(word_to_guess), 1):
-        if word_to_guess[i] == guess[0] and i == 0:
-            new_display += guess[0]
-        elif word_to_guess[i] == guess[0]:
-            new_display += guess[0]
+#adds the numeric value of two strings together
+def adding_str(str1, str2):
+    return 0
+
+#determines whether a string is a palindrome or not
+def palindrome(str):
+    return 0
+
+#determine whether the sum of all the numbers in arr1 equal the product of all the numbers in arr2
+def balanced(arr1, arr2):
+    return 0
+ 
+def asserter(test_case, exp, func, *args):
+    try:
+        ret = func(*args)
+        if ret == exp:
+            print("Test Case " + str(test_case) + " SUCCESS!")
         else:
-            new_display += display_word[i]
-    display_word = new_display
-print("CONGRATS! The word was: " + word_to_guess)
+           print("Test Case " + str(test_case) + " FAILED. We expected: " + str(exp) + " but you returned " + str(ret))
+    except Exception as e:
+        print("Test Case " + str(test_case) + f" had an exception: {type(e).__name__}: {e}")
 
-    
+
+def run_tests():
+    asserter(1.1, "222", adding_str, "111", "111")
+    asserter(1.2, "1345", adding_str, "1000", "345")
+    asserter(1.3, "123", adding_str, "0", "123")
+    asserter(2.1, True, palindrome, "racecar")
+    asserter(2.2, True, palindrome, "i did did i")
+    asserter(2.3, False, palindrome, "notpalindrome")
+    asserter(2.4, False, palindrome, "rotatior")
+    asserter(3.1, True, balanced, [1,2,3], [2,3])
+    asserter(3.2, True, balanced, [0], [0])
+    asserter(3.3, False, balanced, [1,2,3,4], [1,2,3,4])
--- scratchwork2.py
+++ scratchwork2.py
@@ -1,2 +1,31 @@
-dict = {"hello" : "hi", 1 : 2}
-dict.update()
+    
+import time
+import os
+import random
+
+#this generate a set of colors for the users to memorize
+def generate_move(current_turn):
+    bank = ["red", "blue", "green", "yellow"]
+    order = []
+    for i in range(0, current_turn):
+        move = bank[random.randrange(0,4)]
+        print(move)
+        order.append(move)
+        time.sleep(2)
+
+        #develop a system to store the outputs (hint data structure!)
+
+    #clears terminal
+    os.system('cls' if os.name == 'nt' else 'clear')
+    return order
+
+    #likely want to return the data structure
+
+#your code here!
+def play():
+    #start on turn 1
+    turn = 1
+move = generate_move(3)
+time.sleep(2)
+print(move)
+
