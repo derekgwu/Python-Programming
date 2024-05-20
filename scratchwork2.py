a930ce3a5a8cf98ebbd557bc1e3fbce8f4f8751d
Derek Chen <dchen36@gwmail.gwu.edu>
2024-05-20 17:55:11 -0400

    this week exercises

--- scratchwork2.py
+++ scratchwork2.py
@@ -1,23 +1,52 @@
 #calculate the n-th number in the fibonacci sequence
 def fibonacci(n):
-    0
+    first_prev = 1
+    second_prev = 0
+    current =  0
+    for i in range(2,n):
+        current = first_prev  + second_prev
+        second_prev = first_prev
+        first_prev = current
+    return current
 
 #calculate the result of a base raised to some power
 def power(base, power):
-    0
+    ans = 1
+    for i in range(0,power):
+        ans = ans * base
+    return ans
+
 
 #let arr be an array of 0s and 1s symbolizing dominos. Let 0 symbolize no domino and 1 symbolize
 # a domino. Calculate the start index where we can knock over the most number dominos in one go
 def dominos(arr):
     0
 
+
 #given an arr, reverse it and return the new array
 def reverse(arr):
-    0
+    
+    left = 0
+    right = len(arr) - 1
+
+    while left < right:
+        temp = arr[left]
+        arr[left] = arr[right]
+        arr[right] = temp
+        left += 1
+        right -= 1
+    
+    return arr
 
 #given an arr, alter the values such that it is all values in odd indices followed by values in even indices
 def odd_to_even_2(arr):
     0
+    
+def factorial(num):
+    return 0
+
+def div_and_less():
+    return 0
 
 
 
@@ -47,6 +76,21 @@ def run_tests():
     asserter(3.3, 1, dominos, [0,1,1,1,0,0,1,1])
     asserter(4.1, [1,2,3,4,5], reverse, [5,4,3,2,1])
     asserter(5.1, [1,3,5,7,0,2,4,6], odd_to_even_2, [0,1,2,3,4,5,6,7])
+    asserter(6.1, 120, factorial, 5)
+    asserter(6.2, 6, factorial, 3)
+    asserter(7.1, [7,14,21, 28], div_and_less)
 run_tests()
 
     
+""""
+for i to len(arr):
+    highest_idx = -1
+    if arr[i] == 1:
+        j = i
+        while(arr[j] == 1):
+            streak += 1
+            j += 1
+        if highest < streak:
+            highest = streak
+            highest_idx = i
+"""
